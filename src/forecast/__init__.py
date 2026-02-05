12131df1564cff02cad6857acdca43504b6e981d
jonkmatsumo <jonkmatsumo@gmail.com>
2026-02-05 01:01:02 -0500

    refactor(api): add bounded package skeletons for forecast, rules management, and training

--- src/forecast/__init__.py
+++ src/forecast/__init__.py
@@ -1,2 +1,5 @@
-from .services import SignalForecaster, get_forecaster
-from .model_manager import ModelManager, get_model_manager
+from .services import SignalForecaster
+from .model_manager import ModelManager
+from .di import get_forecaster, get_model_manager, get_drift_cache
+
+__all__ = ["SignalForecaster", "ModelManager", "get_forecaster", "get_model_manager", "get_drift_cache"]
--- src/rules_management/__init__.py
+++ src/rules_management/__init__.py
@@ -1,5 +1,8 @@
 from .rules import Rule, RuleSet, RuleStatus
 from .rule_store import RuleStore
-from .draft_store import get_draft_store
-from .versioning import get_version_store
-from .metrics import get_metrics_collector
+from .di import get_draft_store, get_version_store, get_metrics_collector, get_backtest_store
+
+__all__ = [
+    "Rule", "RuleSet", "RuleStatus", "RuleStore",
+    "get_draft_store", "get_version_store", "get_metrics_collector", "get_backtest_store"
+]
